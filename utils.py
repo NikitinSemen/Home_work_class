2c320a094fc9d8eb6c5044ce2be07727be7bdc0a
NikitinSemen <pr.pypblrka@gmail.com>
2024-02-18 15:58:22 +0400

    Dockstring

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from Class import Product
 
 
 def get_json(file_name):
+    """Функция распаковывающая файл json в класс Product"""
     with open(file_name) as file:
         files = json.load(file)
         for f in files:
