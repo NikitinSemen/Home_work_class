7bf0b05b4e1d9ddac61775a50a9e4a08635ab306
NikitinSemen <pr.pypblrka@gmail.com>
2024-02-20 13:24:27 +0400

    созданы методы класса Category:add_products, list_products созданы методы класса Product: get_name, get_descrition, get_price, get_quantity, new_product, change_price

--- category_products.py
+++ category_products.py
@@ -10,8 +10,42 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
-    def metod(self):
+
+    def get_name(self):
         return self.name
+
+    def get_description(self):
+        return self.description
+
+    def get_price(self):
+        return self.price
+
+    def get_quantity(self):
+        return self.quantity
+
+    @classmethod
+    def new_product(cls, value):
+        name, description, price, quantity = (value['name'], value['description'], value['price'], value['quantity'])
+        return cls(name, description, price, quantity)
+
+    @property
+    def change_price(self):
+        return self.price()
+
+    @change_price.setter
+    def change_price(self, value):
+        if value <= 0:
+            print("Введена неккоректная цена")
+        elif self.price > value:
+            while True:
+                user_input = input("Введенная цена ниже установленной, уверены что хотите понизить цену?(y/n)").lower()
+
+                if user_input == 'y':
+                    self.price = value
+                    break
+                elif user_input == 'n':
+                    break
+
 
 class Category:
     """Класс Категории товаров с атрибутами :
@@ -30,9 +64,22 @@ class Category:
                  products: list[Product]):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.total += 1
         for product in products:
             Category.unique_products.add(product.name)
         Category.unique_products_count = len(Category.unique_products)
 
+    def add_product(self, obj_product):
+        self.__products.append(obj_product)
+        Category.total += 1
+
+    def get_products(self):
+        return self.__products
+
+    @property
+    def list_products(self):
+        list_prod = []
+        for p in self.__products:
+            list_prod.append(f'{p.name}, {p.price}руб. Остаток{p.price}')
+            return "".join(list_prod)
