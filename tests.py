c4fc059324ec0d613d6fb0caadd5a55db72e3330
NikitinSemen <pr.pypblrka@gmail.com>
2024-02-20 13:24:40 +0400

    заполнены тесты

--- tests.py
+++ tests.py
@@ -15,9 +15,25 @@ def test_init(product_iphone):
     assert product_iphone.quantity == 12
 
 
+def test_get_name(product_iphone):
+    assert product_iphone.get_name() == 'iphone'
+
+
+def test_get_description(product_iphone):
+    assert product_iphone.get_description() == 'smartphone i not have takou'
+
+
+def test_get_price(product_iphone):
+    assert product_iphone.get_price() == 12000.0
+
+
+def test_quantity(product_iphone):
+    assert product_iphone.get_quantity() == 12
+
+
 @pytest.fixture()
 def category():
-    return Category('Смартфоны', 'телефоны с камерой и интернетом', products=[smartphone, home_phone])
+    return Category('Смартфоны', 'телефоны с камерой и интернетом', products=[smartphone.get_name(), home_phone.get_name()])
 
 
 smartphone = Product('iphone', '', 1212, 12)
@@ -48,4 +64,7 @@ def test_unique_products_count():
 def test_init_category(category):
     assert category.name == 'Смартфоны'
     assert category.description == 'телефоны с камерой и интернетом'
-    assert category.products == category.products
+    # assert category.products == ['iphone', 'iphone']
+
+# def test_list_products(category):
+# assert category.get_products() ==
